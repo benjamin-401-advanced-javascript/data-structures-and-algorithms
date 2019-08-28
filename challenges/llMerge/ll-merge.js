4f8ecd294041934a4d517301233cd03f6079dedf
Benjamin Clark <benisin@gmail.com>
2019-08-28 09:33:31 -0700

    ll-merge.test

--- challenges/llMerge/ll-merge.js
+++ challenges/llMerge/ll-merge.js
@@ -1,7 +1,38 @@
 'use strict'
 
 
+// merge lists
+module.exports = (list1, list2) => {
+  let current1 = list1.head;
+  let current2 = list2.head;
 
-function mergeLists(list1, list2) {
+  if (!current1) {
+    list1.head = current2;
+    return list1;
+  }
 
+  let store1 = current1.next;
+  let store2 = current2.next;
+  let flag = true;
+
+  while (flag) {
+
+    if (!current2) {
+      flag = false;
+      break;
+    }
+    store1 = current1.next;
+    current1.next = current2;
+    current1 = store1;
+
+    if (!current1) {
+      flag = false;
+      break;
+    }
+    store2 = current2.next;
+    current2.next = current1;
+    current2 = store2;
+  }
+
+  return list1;
 }
--- challenges/llMerge/ll-merge.test.js
+++ challenges/llMerge/ll-merge.test.js
@@ -1,15 +1,22 @@
 'use strict';
 
-const s_q = require('./stacks-and-queues')
-const Queue = s_q.queue;
-const Stack = s_q.stack;
+const LL = require('../linkedList/linked-list')
+const merge = require('./ll-merge')
 
-describe('Tests Stack and Queues functionality', () => {
+describe('Tests merge 2 linked lists functionality', () => {
 
-  it('Can successfully merge two stacks', () => {
-    const stack = new Stack();
-    stack.push('happy');
-    expect(stack.top).toEqual('happy');
+  it('Can successfully merge two linked lists', () => {
+    let list1 = new LL();
+    list1.insert(1)
+    list1.insert(3)
+    list1.insert(5)
+    let list2 = new LL();
+    list2.insert(2)
+    list2.insert(4)
+    list2.insert(6)
+    list1 = merge(list1, list2)
+
+    expect(list1).toBe('1,2,3,4,5,6');
   })
 
 })
